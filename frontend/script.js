24872f10967f244f0fdaf0d28faa0e98f45be20c
Ixchel Chavez <ix.chavez@nazaret.team>
2025-10-28 12:01:00 +0100

    Desarrollo del navbar y la portada del sitio

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,21 @@
+// Texto que se va a escribir
 const texto = "Conferencias a las que puedes asistir";
 let i = 0;
 
+// Selecciona el span del HTML
+const textoElemento = document.getElementById("texto");
+
+// Crea el cursor parpadeante
+const cursor = document.createElement("span");
+cursor.classList.add("cursor");
+textoElemento.appendChild(cursor);
+
 function escribir() {
   if (i < texto.length) {
-    document.getElementById("texto").textContent += texto.charAt(i);
+    // Inserta una letra antes del cursor
+    cursor.insertAdjacentText("beforebegin", texto.charAt(i));
     i++;
-    setTimeout(escribir, 100);
+    setTimeout(escribir, 70); // Velocidad de escritura
   }
 }
 

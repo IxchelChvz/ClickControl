ded2335ee408fc54f223ecec10f46fce693e6561
Ixchel Chavez <ixchechvz2002@gmail.com>
2025-10-30 09:47:49 +0100

    Cambio de logo, video de fondo y navbar

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,20 @@
-// Texto que se va a escribir
+
 const texto = "Conferencias a las que puedes asistir";
 let i = 0;
 
-// Selecciona el span del HTML
+
 const textoElemento = document.getElementById("texto");
 
-// Crea el cursor parpadeante
+
 const cursor = document.createElement("span");
 cursor.classList.add("cursor");
 textoElemento.appendChild(cursor);
 
 function escribir() {
   if (i < texto.length) {
-    // Inserta una letra antes del cursor
     cursor.insertAdjacentText("beforebegin", texto.charAt(i));
     i++;
-    setTimeout(escribir, 70); // Velocidad de escritura
+    setTimeout(escribir, 70); 
   }
 }
 
